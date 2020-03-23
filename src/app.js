92704a560d34d2845a00eab42f3bbc8cc13f2082
Faigy Roze <faywiesel@gmail.com>
2020-03-22 20:48:46 -0400

    heroku environment vars

--- src/app.js
+++ src/app.js
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-
-
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
+import "./firebase/firebase";
 
 const store = configureStore();
 
